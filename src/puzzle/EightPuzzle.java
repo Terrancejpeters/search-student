dde9a33b3e1bc0083e571d657adeb202e4d50d84
Terrance J Peters <terrancejpeters@gmail.com>
2016-04-22 02:39:37 -0400

    I am in pain

--- src/puzzle/EightPuzzle.java
+++ src/puzzle/EightPuzzle.java
@@ -49,6 +49,7 @@ import search.Solver;
 public class EightPuzzle implements SearchProblem<List<Integer>> {
 	
 	private List<Integer> initial;
+	private List<Integer> solved;
 	/**
 	 * Creates a new instance of the 8 puzzle with the given starting values.
 	 * 
@@ -63,6 +64,8 @@ public class EightPuzzle implements SearchProblem<List<Integer>> {
 	public EightPuzzle(List<Integer> startingValues) {
 		// TODO
 		initial = startingValues;
+		solved = Arrays.asList(new Integer[] { 1, 2, 3,
+				4, 5, 6, 7, 8, 0 });
 	}
 
 	@Override
@@ -74,30 +77,94 @@ public class EightPuzzle implements SearchProblem<List<Integer>> {
 	@Override
 	public List<List<Integer>> getSuccessors(List<Integer> currentState) {
 		// TODO
-		List<List<Integer>> killme = new ArrayList<List<Integer>>();
+		List<List<Integer>> ListOLists = new ArrayList<List<Integer>>();
 		int pos = currentState.indexOf(0);
+		System.out.println("0 is at " + pos);
 		if (pos == 0){
-			killme.add(swap(0,1,currentState));
-			killme.add(swap(0,3,currentState));
+			ListOLists.add(swap(0,1,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(0,3,currentState));
+			toString(ListOLists.get(1));
 		}
-		if (pos == 1){
-			killme.add(swap(1,2,currentState));
-			killme.add(swap(1,4,currentState));
-			killme.add(swap(1,0,currentState));
+		else if (pos == 1){
+			ListOLists.add(swap(1,2,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(1,4,currentState));
+			toString(ListOLists.get(1));
+			ListOLists.add(swap(1,0,currentState));
+			toString(ListOLists.get(2));
 		}
-		return null;
+		else if (pos == 2){
+			ListOLists.add(swap(2,1,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(2,5,currentState));
+			toString(ListOLists.get(1));
+		}
+		else if (pos == 3){
+			ListOLists.add(swap(3,0,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(3,4,currentState));
+			toString(ListOLists.get(1));
+			ListOLists.add(swap(3,6,currentState));
+			toString(ListOLists.get(2));
+		}
+		else if (pos == 4){
+			ListOLists.add(swap(4,1,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(4,3, currentState));
+			toString(ListOLists.get(1));
+			ListOLists.add(swap(4,5,currentState));
+			toString(ListOLists.get(2));
+			ListOLists.add(swap(4,7,currentState));
+			toString(ListOLists.get(3));
+		}
+		else if (pos == 5){
+			ListOLists.add(swap(5,2,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(5,4,currentState));
+			toString(ListOLists.get(1));
+			ListOLists.add(swap(5,8,currentState));
+			toString(ListOLists.get(2));
+		}
+		else if (pos == 6){
+			ListOLists.add(swap(6,3,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(6,7,currentState));
+			toString(ListOLists.get(1));
+		}
+		else if (pos == 7){
+			ListOLists.add(swap(7,4,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(7,6,currentState));
+			toString(ListOLists.get(1));
+			ListOLists.add(swap(7,8,currentState));
+			toString(ListOLists.get(2));
+		}
+		else if (pos == 8){
+			ListOLists.add(swap(8,5,currentState));
+			toString(ListOLists.get(0));
+			ListOLists.add(swap(8,7,currentState));
+			toString(ListOLists.get(1));
+		}
+		for (int i = 0; i < ListOLists.size(); i++){
+			System.out.println(ListOLists.get(i));
+		}
+		return ListOLists;
 	}
 	
-	private List<Integer> swap(int a, int b, List<Integer> pain){
-		int temp = pain.get(a);
-		pain.set(a, pain.get(b));
-		pain.set(b, temp);
-		return pain;
+	private List<Integer> swap(int a, int b, List<Integer> input){
+		List<Integer> tList = input;
+		int temp = tList.get(a);
+		tList.set(a, tList.get(b));
+		tList.set(b, temp);
+		return tList;
 	}
 
 	@Override
 	public boolean isGoal(List<Integer> state) {
-		// TODO
+		if (state.equals(solved)){
+			return true;
+		}
 		return false;
 	}
 
@@ -109,5 +176,12 @@ public class EightPuzzle implements SearchProblem<List<Integer>> {
 		for (List<Integer> l : r) {
 			System.out.println(l);
 		}
+	}
+	
+	public void toString(List<Integer> state){
+		System.out.println(" | " +state.get(0) + " | " + state.get(1) +" | "  + state.get(2)+ " | ");
+		System.out.println(" | " +state.get(3) + " | " + state.get(4) +" | " + state.get(5)+  " | " );
+		System.out.println(" | " +state.get(6) + " | " + state.get(7) +" | " +  state.get(8) + " | ");
+		System.out.println();
 	}
 }
